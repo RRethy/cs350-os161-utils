c52b240dbda3ae9aca7e08991a5a61c967cdac51
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-30 09:49:27 -0500

    Fix another stupid mistake

--- lib/os161/colour.rb
+++ lib/os161/colour.rb
@@ -2,15 +2,15 @@ module OS161
   class Colour
     class << self
       def blue(str)
-        return "`tput setaf 4`#{msg}#{reset}"
+        return "`tput setaf 4`#{str}#{reset}"
       end
 
       def green(str)
-        return "`tput setaf 2`#{msg}#{reset}"
+        return "`tput setaf 2`#{str}#{reset}"
       end
 
       def red(str)
-        return "`tput setaf 1`#{msg}#{reset}"
+        return "`tput setaf 1`#{str}#{reset}"
       end
 
       private
