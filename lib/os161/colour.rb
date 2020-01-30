192ece7ea083a8e72c289152afd257762cb65c7e
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-30 11:37:55 -0500

    Use escape sequences instead of tput for colour

--- lib/os161/colour.rb
+++ lib/os161/colour.rb
@@ -2,21 +2,22 @@ module OS161
   class Colour
     class << self
       def blue(str)
-        return "`tput setaf 4`#{str}#{reset}"
+        return "\e[34m#{str}#{reset}"
       end
 
       def green(str)
+        return "\e[32m#{str}#{reset}"
         return "`tput setaf 2`#{str}#{reset}"
       end
 
       def red(str)
-        return "`tput setaf 1`#{str}#{reset}"
+        return "\e[31m#{str}#{reset}"
       end
 
       private
 
       def reset
-        return "`tput sgr0`"
+        return "\e[37m"
       end
     end
   end
