ee5849eeb3d0f527149c37585e3ae16913cf6919
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-31 16:50:53 -0500

    Fix usage of singleton in run.rb

--- lib/os161/commands/run.rb
+++ lib/os161/commands/run.rb
@@ -7,7 +7,7 @@ module OS161
     class Run
       class << self
         def call
-          version = Options.assn_nr
+          version = Options.instance.assn_nr
           if version.nil?
             kernel = "kernel"
           else
