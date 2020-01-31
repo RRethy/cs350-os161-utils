c8eac0a4b3461558622263a74af5eac2579f3ea3
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-31 17:05:35 -0500

    Update --help

--- lib/os161/commands/help.rb
+++ lib/os161/commands/help.rb
@@ -7,14 +7,26 @@ module OS161
 os161util [OPTION]
 
 os161util is a utility command to help with the development of os161 in CS350 at
-UWaterloo. TODO.
+UWaterloo.
+
+Options can be combined and ordering with no get wack; building will always be
+done before running anything. For example, `--run --build` will build the
+kernel, then run the simulator.
 
 OPTIONS:
 --help, -h:
     show help
---build version, -b version:
---run version, -r version:
-          EOF
+--build, -b:
+    build the kernel specified by -r (which has a good default heuristic).
+--run, -r:
+    run the simulator with the specified version or using the same heuristic as
+    --build.
+--assignment_number integer, -r integer:
+    specifies which kernel version to build and use.
+    By default it'll use a heuristic based on hardcoded assignment deadlines
+    for W20 to determine which version of the kernel to build. You can specify
+    a specific kernel version with --assignment_number (-r).
+EOF
         end
       end
     end
