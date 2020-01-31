c0c96bad48989f70ba33e3d3a54f847262025cf5
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-31 16:54:58 -0500

    Actually set assn_nr in heuristic lol

--- lib/os161/options.rb
+++ lib/os161/options.rb
@@ -17,10 +17,15 @@ module OS161
     def assn_nr_heuristic
       today = Date.today
       if today <= Date.new(2020, 1, 17)
+        0
       elsif today <= Date.new(2020, 2, 3)
+        1
       elsif today <= Date.new(2020, 2, 24)
+        2
       elsif today <= Date.new(2020, 3, 16)
+        3
       elsif today <= Date.new(2020, 4, 1)
+        4
       else
         puts Colour::red("Hello future humans.\nI can't figure out what assignment you are working on because I was built using the deadlines from W20.\nPlease submit a patch at github.com/RRethy/cs350-os161-utils to the file ./lib/os161/options.rb.\nOr specify the --assignment_number (-n) option.")
         exit(1)
