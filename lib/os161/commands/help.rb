da4270cbf782f3de4e8b0ffd38296a807e9d5ccf
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-01-31 17:17:15 -0500

    Update --help

--- lib/os161/commands/help.rb
+++ lib/os161/commands/help.rb
@@ -6,8 +6,11 @@ module OS161
           puts <<-EOF
 os161util [OPTION]
 
-os161util is a utility command to help with the development of os161 in CS350 at
+os161 is a utility command to help with the development of os161 in CS350 at
 UWaterloo.
+
+os161 will execute all your commands without fucking up your current working
+directory, you also won't have to cd everywhere because that sucks shit.
 
 Options can be combined and ordering will not get wack; building will always be
 done before running anything. For example, `--run --build` will build the
@@ -18,9 +21,12 @@ OPTIONS:
     show help
 --build, -b:
     build the kernel specified by -r (which has a good default heuristic).
---run, -r:
-    run the simulator with the specified version or using the same heuristic as
-    --build.
+--run <cmd>, -r <cmd>:
+    run the simulator with the kernel specified by -r (which has a good default heuristic).
+    <cmd> can be passed and the simulator will run it and exit.
+    These would be pretty much the same:
+    $ os161 -r "sp3 1 150 1 1 0"
+    $ sys161 kernel "sp3 1 150 1 1 0;q"
 --assignment_number integer, -r integer:
     specifies which kernel version to build and use.
     By default it'll use a heuristic based on hardcoded assignment deadlines
