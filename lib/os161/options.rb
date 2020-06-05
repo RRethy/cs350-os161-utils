c1e64c21b30b6d20204bc8639097e6214226dc6b
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2020-06-05 13:52:07 -0400

    Update assn deadlines

--- lib/os161/options.rb
+++ lib/os161/options.rb
@@ -21,15 +21,15 @@ module OS161
 
     def assn_nr_heuristic
       today = Date.today
-      if today <= Date.new(2020, 1, 17)
+      if today <= Date.new(2020, 6, 5)
         '0'
-      elsif today <= Date.new(2020, 2, 3)
+      elsif today <= Date.new(2020, 6, 10)
         '1'
-      elsif today <= Date.new(2020, 2, 24)
+      elsif today <= Date.new(2020, 6, 26)
         '2a'
-      elsif today <= Date.new(2020, 3, 16)
+      elsif today <= Date.new(2020, 7, 10)
         '2b'
-      elsif today <= Date.new(2020, 4, 1)
+      elsif today <= Date.new(2020, 7, 1)
         '3'
       else
         puts Colour::red("Hello future humans.\nI can't figure out what assignment you are working on because I was built using the deadlines from W20.\nPlease submit a patch at github.com/RRethy/cs350-os161-utils to the file ./lib/os161/options.rb.\nOr specify the --assignment_number (-n) option.")
